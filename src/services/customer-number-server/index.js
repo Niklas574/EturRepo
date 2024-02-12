21341997ab08b97d90b9faae8be10aa1cd0b7604
SaraGorodezki <sara.gorodezki@plan-b-gmbh.com>
2024-02-12 16:57:34 +0100

    import new function

--- src/services/customer-number-server/index.js
+++ src/services/customer-number-server/index.js
@@ -1,5 +1,6 @@
 const message = "Hello World!";
 console.log(message);
 
-import {printCustomers} from './customer.js'
+import {printCustomers, createCustomer} from './customer.js'
+console.log(createCustomer("Justin", 22, "justin@gmail.com", "01234567", "K0003" ));
 console.log(printCustomers());
