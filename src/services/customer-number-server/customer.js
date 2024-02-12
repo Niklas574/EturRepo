932508501aef73975fe6b8384d7908df3dbffca6
Niklas574 <Niklas.Dogan@plan-b-gmbh.com>
2024-02-12 16:08:27 +0100

    customer

---
+++ src/services/customer-number-server/customer.js
@@ -0,0 +1,22 @@
+const customer1 = {
+    name: "Sara", 
+    age: 22, 
+    email: "sara@gmail.com", 
+    phoneNumber: "015110284253", 
+    customerNumber: "K0001"
+}
+
+const customer2 = {
+    name: "Niklas", 
+    age: 16, 
+    email: "niklas@gmail.com", 
+    phoneNumber: "015752781728", 
+    customerNumber: "K0002"
+}
+
+const customers = [{customer1}, {customer2}];
+function print(customers)
+{
+    console.log(customers);
+}
+print(customers);
