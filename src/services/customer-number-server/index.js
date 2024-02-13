32deb178c241873bd7fd0dc4fcaebcb9bfc9b745
SaraGorodezki <sara.gorodezki@plan-b-gmbh.com>
2024-02-13 09:42:59 +0100

    server

--- src/services/customer-number-server/index.js
+++ src/services/customer-number-server/index.js
@@ -1,13 +1,39 @@
 //const message = "Hello World!";
 //console.log(message);
+import Fastify from "fastify";
+
+const fastify = Fastify({
+    logger: true
+})
+
+try{
+    await fastify.listen(4040);
+} catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+}
+ 
+const getOptions = {
+    schema: {
+        response: {
+            200: {
+                type: "object",
+                proberties: {
+                    hello: { type: "string" },
+
+                },
+            },
+        },
+    },
+};
+
+fastify.get("/Kunden", getOptions, (request, reply) => {
+    reply.send({ hello: "world"});
+});
 
 import {printCustomers, createCustomer, getCustomerWithId, deleteCustomer, getRandomInt} from './customer.js'
-//console.log(createCustomer("Justin", 22, "justin@gmail.com", "01234567", "K0003" ));
-//console.log(printCustomers());
-//console.log(getCustomerWithId("K0003"));
-//deleteCustomer("K0003");
-//console.log("-------------");
-//console.log(printCustomers());
+
+
 
 createCustomer("Justin", 22, "justin@gmail.com", "01234567", "ETUR-CN-3" );
 createCustomer("David", 18, "david@gmail.com", "1234567", "ETUR-CN-4");
